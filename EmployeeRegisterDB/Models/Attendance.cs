1059650bc0b611f18fe626d6847117f7c72dbea5
bkwrdbailey <Baileybarton77@yahoo.com>
2022-12-18 21:52:21 -0600

    Changed class model Attendance's dateCreated data type

--- EmployeeRegisterDB/Models/Attendance.cs
+++ EmployeeRegisterDB/Models/Attendance.cs
@@ -7,6 +7,6 @@ public class Attendance
     public ObjectId id { get; set; }
     public int empId { get; set; }
     public string? attendanceCode { get; set; }
-    public DateTime dateCreated { get; set; }
+    public string dateCreated { get; set; }
     public string? leaveType { get; set; }
 }
--- EmployeeRegisterDB/Models/EmployeeTabularData.cs
+++ EmployeeRegisterDB/Models/EmployeeTabularData.cs
@@ -4,7 +4,7 @@ public class EmployeeTabularData
 {
     public string? name { get; set; }
     public int empId { get; set; }
-    public DateTime date { get; set; }
+    public string date { get; set; }
     public string? attendanceCode { get; set; }
     public string? leaveType { get; set; }
     public int managerId { get; set; }
